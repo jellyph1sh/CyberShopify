58fd28410e5ec6bd5921d57ca21617e393acaeee
Adrien <adrien.poirier@oui-connect.fr>
2023-02-06 10:22:40 +0100

    next JS simple page

--- components/Layout.js
+++ components/Layout.js
@@ -1,48 +1,50 @@
-import React from 'react';
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
-function Layout() {
+const products = [
+  { id: 1, name: 'Product 1', price: 29.99, image: '/product1.jpg' },
+  { id: 2, name: 'Product 2', price: 39.99, image: '/product2.jpg' },
+  { id: 3, name: 'Product 3', price: 49.99, image: '/product3.jpg' },
+  { id: 4, name: 'Product 4', price: 59.99, image: '/product4.jpg' },
+  { id: 5, name: 'Product 5', price: 69.99, image: '/product5.jpg' }
+]
+
+export default function Home() {
   return (
-    <div className="App">
-      <header className="header">
-        <nav className="navbar">
-          <a href="#">Home</a>
-          <a href="#">Shop</a>
-          <a href="#">About</a>
+    <div className={styles.container}>
+      <Head>
+        <title>Site de vente</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" />
+      </Head>
+
+      <header className={styles.header}>
+        <h1>Site de vente</h1>
+        <nav className={styles.navbar}>
+          <Link href="#">Accueil</Link>
+          <Link href="#">Produits</Link>
+          <Link href="#">Contact</Link>
         </nav>
-        <form className="search-form">
-          <input type="text" placeholder="Search..." />
-          <button type="submit">Search</button>
-        </form>
       </header>
-      <main className="main">
-        <h1>Welcome to our store!</h1>
-        <p>Here are our latest products:</p>
-        <div className="products">
-          <div className="product">
-            <img src="#" alt="Product" />
-            <h2>Product name</h2>
-            <p>$price</p>
-            <button>Add to Cart</button>
-          </div>
-          <div className="product">
-            <img src="#" alt="Product" />
-            <h2>Product name</h2>
-            <p>$price</p>
-            <button>Add to Cart</button>
-          </div>
-          <div className="product">
-            <img src="#" alt="Product" />
-            <h2>Product name</h2>
-            <p>$price</p>
-            <button>Add to Cart</button>
-          </div>
+
+      <main className={styles.main}>
+        <h2>Nos produits</h2>
+        <div className={styles.products}>
+          {products.map(product => (
+            <div key={product.id} className={styles.product}>
+              <img src={product.image} alt={product.name} />
+              <h2>{product.name}</h2>
+              <p>{product.price} €</p>
+              <button>Ajouter au panier</button>
+            </div>
+          ))}
         </div>
       </main>
-      <footer className="footer">
-        <p>Copyright &copy; 2023</p>
+
+      <footer className={styles.footer}>
+        <p>&copy; {new Date().getFullYear()} Site de vente</p>
       </footer>
     </div>
-  );
+  )
 }
-
-export default Layout;

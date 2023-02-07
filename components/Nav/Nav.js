eb25e033e5a68653205c7d45ff4cb0728e0787d2
ckevin <kevin.chaffaux@ynov.com>
2023-02-07 16:35:54 +0100

    :lipstick: | fix design login & register

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -19,7 +19,7 @@ const Nav = () => {
                 <nav className={styles.navbar}>
                     <ul>
                         <Link className={styles.navbarLi} href="/">Accueil</Link>
-                        <Link className={styles.navbarLi} href="">Contact</Link>
+                        <Link className={styles.navbarLi} href="/contact">Contact</Link>
                         {/* <Link className={styles.navbarLi} href="/commande">Commande</Link> */}
                         <Link className={styles.navbarLi} href="/login">Login</Link>
                         <Link className={styles.navbarLi} href="/register">Register</Link>
